3d85d5363cc02e46152b47cd963f8385ccc8aa7b
basha105 <brentbasha17@gmail.com>
2024-09-09 21:32:30 -0400

    Working on it

--- src/createList.js
+++ src/createList.js
@@ -11,16 +11,24 @@ function createList(myTitle) {
     const removeItem = (selectedItem) => {
         for (let i=0; i<listLength; i++) {
             if (selectedItem === todoItems[i]) {
-                todoItems.splice(i, i);
+                todoItems.splice(i, 1);
+                listLength--;
             }
         }
     }
 
     const getList = () => todoItems;
 
+    const displayItems = () => {
+        console.log(`${title}'s items:`)
+        for (let i=0; i<listLength; i++) {
+            console.log(todoItems[i].title);
+        }
+    }
+
     const getListLength = () => listLength;
 
-    return {title, addItem, removeItem, getList, getListLength}
+    return {title, addItem, removeItem, getList, displayItems, getListLength}
 }
 
 export { createList };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import "./styles.css"
 import { createTodo } from "./createTodo.js";
 import { createList } from "./createList.js";
 
-let defaultList = createList("default");
-
-let waterLawn = createTodo("lawn");
-
-defaultList.addItem(waterLawn);
+let defaultList = createList("defaultList");
 
 
 
