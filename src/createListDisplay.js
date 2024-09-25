31ffcd3a2583d105ca9aa1e206bc5d39c1d46303
basha105 <brentbasha17@gmail.com>
2024-09-25 16:27:38 -0400

    Trying to get display working

---
+++ src/createListDisplay.js
@@ -0,0 +1,18 @@
+export default function createListDisplay(currentList) {
+    const listContainer = document.createElement("div");
+    const header = document.createElement("h1");
+    header.textContent = `${currentList.title}`;
+    listContainer.appendChild(header);
+
+    const list = document.createElement("ul");
+    for (let i=0; i<currentList.getListLength(); i++) {
+        let newListItem = document.createElement("li");
+        newListItem.textContent = currentList.getList()[i].title;
+        list.appendChild(newListItem);
+    }
+
+    // Make an html list of items, with each item's text content being the title of the todo item
+
+    listContainer.appendChild(list);
+    return listContainer;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import "./styles.css"
 import { createTodo } from "./createTodo.js";
 import { createList } from "./createList.js";
+import { createListDisplay } from "./createListDisplay.js"
 
-let defaultList = createList("defaultList");
+const listContainer = document.getElementById("listDisplay");
+let defaultList = createList("default");
+
+let washDishes = createTodo("Wash the dishes");
+defaultList.addItem(washDishes);
+
+let defaultDisplay = createListDisplay(defaultList);
+listContainer.appendChild(defaultDisplay);
+
+
+
 
 
 
